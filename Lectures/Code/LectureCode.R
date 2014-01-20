b22863c046c1dd33b4b2857f1a7ceed5b32dae8e
Måns Magnusson <mons.magnusson@gmail.com>
2014-01-20 16:57:14 +0100

    Added data in Lecture code

--- Lectures/Code/LectureCode.R
+++ Lectures/Code/LectureCode.R
@@ -1,4 +1,4 @@
-# Internal functions for demonstrations etc.
+# Internal functions, simulated data for demonstrations etc.
 
 
 # Calculate estimates with standard errors with simple random sampling (SRS)
@@ -19,3 +19,40 @@ pSRS <- function(n, N, p){
                   SE = sqrt(p*(1-p)*(1/(n-1))*(1-n/N)))
                   return(results)
 }
+
+
+# Data for examples in Lectures
+# Gastroenteritis example F4
+generateGastro <- function(mySeed){
+  set.seed(mySeed) 
+  # Create example
+  N <- c(1687283, 1851959, 1812691, 2935231)
+  n <- round(400.1*(gastro$N/sum(gastro$N)))
+  
+  e.y<-c(3.5,0.7,1.8,0.4) 
+  age<-c(0,15,25,40,65) 
+  y.mean<-numeric(length(n)) 
+  y.s<-numeric(length(n))
+  for(i in 1:length(n)){   
+    y.temp<-round(rexp(n[i],1/e.y[i]))   
+    y.mean[i]<-round(mean(y.temp),2)  
+    y.s[i]<-round(sd(y.temp),2) 
+    x.temp<-round(runif(n=n[i],min=age[i],max=age[i+1]))   
+    if(i==1){y<-y.temp;x<-x.temp}   
+    if(i>1){y<-c(y,y.temp);x<-c(x,x.temp)} 
+  }
+  gastro$N <- N
+  gastro$n <- n
+  gastro$y <- y
+  gastro$x <- x
+  gastro$age <- age
+  gastro$y_bar <- y.mean
+  gastro$y_bar_all <- round(mean(y),2) 
+  gastro$s_y <- y.s
+  gastro$s_y_all <- round(sd(y),2) 
+  return(gastro) 
+}
+gastro <- generateGastro(20130206)
+
+
+
