061d8dfd1d693f5b1433bc0d75c9813306f04486
Måns Magnusson <mons.magnusson@gmail.com>
2014-01-29 12:42:12 +0100

    Added n

--- Lectures/Code/LectureCode.R
+++ Lectures/Code/LectureCode.R
@@ -55,7 +55,7 @@ plotnsizedata <- function(yPop, n, mySeed){
     sample_s[i] <- sd(SRSsample,na.rm=TRUE)
     sample_se[i] <- ySRS(N=N,n=n[i],ybar=sample_m[i],s2=sample_s[i]^2)$SE
   }
-  samplePlotData <- data.frame(mean=sample_m, s=sample_s ,SE=sample_se)
+  samplePlotData <- data.frame(n = n, mean = sample_m, s = sample_s ,SE = sample_se)
   return(list(popMean = m, popStd = s, samplePlotData = samplePlotData))
 }
 
