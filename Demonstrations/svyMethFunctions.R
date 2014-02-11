87b4fd51a56b60f61174bbc530f9adb69c877dc2
Måns Magnusson <mons.magnusson@gmail.com>
2014-02-11 20:39:28 +0100

    distReg

--- Demonstrations/svyMethFunctions.R
+++ Demonstrations/svyMethFunctions.R
@@ -54,3 +54,41 @@ samplingDistRatio<-function(y,x,n,alpha=0.05){
   return(dist)
 }
 
+
+samplingDistReg<-function(y,x,n,alpha=0.05){
+  # y: vektor av värden (ex. c(5,10,8,7,12))
+  # x: vektor med hjälpvariabelvärden (måste vara lika lång som y)
+  # n: urvalsstorlek n <- 6
+  stopifnot(length(x) == length(y)) 
+  stopifnot(n > 2 & n < length(y)) 
+  
+  no.obs<-1:length(y)
+  N<-length(y)
+  
+  dist <- as.data.frame(t(combn(y, n)))
+  distX <- as.data.frame(t(combn(x, n)))
+  names(dist)[1:n]<-paste("obs.",1:n,sep="")
+  
+  regFun<-function(vec,...) lm(formula=vec[1:n]~vec[(n+1):(2*n)])$coefficients  
+  temp<-t(apply(cbind(dist[,1:n],distX[,1:n]), MARGIN=1, FUN=regFun))
+  colnames(temp)<-c("B0","B1")
+  temp <- as.data.frame(temp)
+  
+  # Beräknar urvalssannolikheten
+  dist$P_S<-1/choose(length(y),n)
+  
+  # Beräknar y_hat, y_hat_r
+  dist$n<-n
+  dist$truemean_x<-mean(x)
+  dist$mean_hat_x<-apply(distX[,1:n],MARGIN=1,FUN=mean)
+  dist$mean_hat_y<-apply(dist[,1:n],MARGIN=1,FUN=mean)
+  dist <- cbind(dist, temp)
+  dist$mean_hat_yreg<-dist$mean_hat_y+dist$B1*(dist$truemean_x-dist$mean_hat_x)
+  
+  return(dist)
+}
+
+# test <- samplingDistReg(y=y, x=x, n=5)
+# 
+# hist(test$mean_hat_yreg)
+# hist(test$mean_hat_y)
